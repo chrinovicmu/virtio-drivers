ede4628f55dc843f255f0aa97d87aa486315c427
ChrinovicMu <chrinovicmu@gmail.com>
2025-09-19 16:29:21 +0200

    virtio-net initialize and cleanup added

--- src/virtio_net.c
+++ src/virtio_net.c
@@ -138,8 +138,114 @@ static void virtio_net_recieve(struct virtio_net_dev *vnet_dev)
         memcpy(dst_addr, buf, len); 
 
         skb->protocol = eth_type_trans(skb, vnet_dev->netdev);
+
+        /*hand over to kernel network stack */ 
         netif_rx(skb);
 
+        /*re-add buffer to RX queue */ 
+        struct scatterlist sg[1]; 
+        sg_init_one(sg, buf, len); 
+        virtqueue_add_inbuf(vq, sg, buf, GFP_ATOMIC); 
+    }
+}
+
+/*initialize virtio-net device */ 
+
+int virtio_net_init(struct virtio_pci_dev *vpci_dev)
+{
+    struct virtio_net_dev *vnet_dev; 
+    struct net_device *netdev; 
+    struct virtio_net_config *net_cfg = (struct virtio_net_config*)vpci_dev->device_cfg;
+    int ret; 
+    /*allocate network device */ 
+    netdev = alloc_etherdev(sizeof(struct virtio_net_dev)); 
+    if(!netdev)
+    {
+        dev_err(&vpci_dev->pdev->dev, "Failed to allocate net device\n"); 
+        return -ENOMEM; 
     }
 
+    /*initialize virtio_net_dev*/ 
+    vnet_dev->netdev_priv(netdev);
+    vnet_dev->vpci_dev = vpci_dev; 
+    vnet_dev->netdev = netdev; 
+
+    /*set network device ops*/
+    netdev->netdev_ops = &virtio_netdev_ops; 
+
+    /*set mac*/
+    memcpy(netdev->dev_addr, net_cfg->mac, ETH_ALEN);
+    netdev->addr_len = ETH_ALEN;
+
+    /*set mtu (if supported)*/
+    if(vpci_dev->guest_features & (1ULL << VIRTIO_NET_F_MTU))
+    {
+        u16 mtu = le16_to_cpu(ioread16(&net_cfg->mtu)); 
+        if(mtu)
+            netdev->mtu = mtu; 
+    }
+
+    /*register network device */ 
+    ret = register_netdev(netdev); 
+    if(ret)
+    {
+        dev_err(&vpci_dev->pdev->dev, "Failed to register network device: %d\n"); 
+        free_netdev(netdev); 
+        return ret; 
+    }
+
+    /*pre-allocate RX buffer*/ 
+    struct virtqueue *rx_vq = vpci_dev->virqs[VIRTIO_NET_QUEUE_RX]; 
+
+    for(int x = 0; x < virtqueue_get_ring_size(rx_vq); ++x)
+    {
+        struct scatterlist sg[1]; 
+        void *buf = kmalloc(ETH_FRAME_LEN, GFP_KERNEL); 
+        if(!buf) 
+        {
+            ret = -ENOMEM;
+            goto err_unregister_netdev; 
+        }
+
+        sg_init_one(sg, buf, ETH_FRAME_LEN); 
+        ret = virtqueue_add_inbuf(rx_vq, sg, 1, buf, GFP_KERNEL);
+        if(ret)
+        {
+            kfree(buf);
+            dev_err(&vpci_dev->pdev->dev, "Failed to add RX buffer: %d"\n, ret); 
+            goto err_free_buffers; 
+        }
+    }
+
+    dev_info(&vpci_dev->pdev->dev, "virtio-net initialized, MAC: %pM\n", netdev->dev_addr);
+
+    return 0; 
+
+err_free_buffers:
+    while((buf = virtqueue_get_buf(rx_vq, NULL)) != NULL)
+        kfree(buf); 
+
+err_unregister_netdev:
+    unregister_netdev(netdev); 
+    free_netdev(netdev);
+    return ret; 
 }
+
+/*cleanup viriot-net device */ 
+void virtio_net_exit(struct virtio_net_dev *vnet_dev)
+{
+    struct virtio_pci_dev *vpci_dev = vnet_dev->vpci_dev; 
+    struct virtqueue *rx_vq = vpci_dev->virtqs[VIRTIO_NET_QUEUE_RX]; 
+    void *buf; 
+
+    /*stop network device */ 
+    netif_stop_queue(vnet_dev->netdev);
+
+    /*free RX buffers */ 
+    while((buf = virtqueue_get_buf(rx_vq, NULL)) != NULL)
+        kfree(buf); 
+
+    unregister_netdev(vnet_dev->netdev); 
+    free_netdev(vnet_dev->netdev); 
+}
+
